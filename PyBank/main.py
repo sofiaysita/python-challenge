aff617e6f1861492b0c06bd4ab71528efeb6d6d5
sofiaysita <sofiaysita3@gmail.com>
2022-09-16 13:34:13 -0500

    Homework 3

---
+++ PyBank/main.py
@@ -0,0 +1,59 @@
+from asyncore import read
+from functools import total_ordering
+import os
+import csv
+#Read CSV file
+budget_csv = os.path.join("Resources","budget_data.csv")
+with open(budget_csv) as csv_file:
+    csv_reader = csv.reader(csv_file,delimiter=",")
+    header_row = next(csv_reader,None)
+    #Set variables and dictionaries to 0
+    months = []
+    profit_loss_changes = []
+    rowcount = 0
+    total = 0
+    curr_month = 0
+    prev_month = 0
+    change_profit_loss = 0
+    # Iterate through rows
+    for row in csv_reader:
+        rowcount +=1
+        total += int(row[1])
+        curr_month = int(row[1])
+        if rowcount == 1:
+            prev_month += curr_month
+        else:
+            change_profit_loss = curr_month - prev_month
+            prev_month = curr_month
+            #Store results
+            months.append(row[0])
+            profit_loss_changes.append(change_profit_loss )
+
+    #Calculate Analysis
+    average = round(sum(profit_loss_changes)/(rowcount-1),2)
+    max_increase = max(profit_loss_changes)
+    min_increase = min(profit_loss_changes)
+    max_index = profit_loss_changes.index(max_increase)
+    min_index = profit_loss_changes.index(min_increase)
+    max_month = months[max_index]
+    min_month = months[min_index]
+    #Print on terminal
+    print("Financial Analysis")
+    print("-------------------------")
+    print("Total Months:", rowcount)
+    print("Total: $",total)
+    print("Average: $",round(average,2))
+    print(f"Greatest Increase in Profits: {max_month} (${max_increase})")
+    print(f"Greatest Increase in Profits: {min_month} (${min_increase})")
+    
+   #Export to txt file
+    txt_path = os.path.join("Analysis", "Financial_Analysis.txt")
+    f = open(txt_path, "x")
+    f.write("Financial Analysis\n")
+    f.write("-------------------------\n")
+    f.write(f"Total Months: {rowcount}\n")
+    f.write(f"Total: ${total}\n")
+    f.write(f"Average: ${average}\n")
+    f.write(f"Greatest Increase in Profits: {max_month} (${max_increase})\n")
+    f.write(f"Greatest Increase in Profits: {min_month} (${min_increase})")
+    f.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,41 @@
+from asyncore import read
+import os
+import csv
+from tkinter import X
+#Read CSV file
+election_csv = os.path.join("Resources","election_data.csv")
+
+with open(election_csv) as csv_file:
+    csv_reader = csv.reader(csv_file,delimiter=",")
+    header_row = next(csv_reader,None)
+    #Set variables and dictionaries to 0
+    candidates = []
+    percentage = []
+    votes = []
+    unique_candidates = []
+    votecount = 0
+    # Iterate through rows
+    for row in csv_reader:
+        votecount +=1
+        candidates.append(row[2])
+    for x in set(candidates):
+        unique_candidates.append(x)
+        votes_per_candidate = candidates.count(x)
+        votes.append(votes_per_candidate)
+        per = (votes_per_candidate/votecount)*100
+        percentage.append(per)
+    max_votes = max(votes)
+    index = votes.index(max_votes)
+    winner = unique_candidates[index]
+    
+                        
+print("Election Results")
+print("----------------------")
+print("Total Votes: ",votecount)
+print("----------------------")
+print(f"{unique_candidates[1]}: {round(percentage[1],3)}% ({votes[1]})")
+print(f"{unique_candidates[2]}: {round(percentage[2],3)}% ({votes[2]})")
+print(f"{unique_candidates[0]}: {round(percentage[0],3)}% ({votes[0]})")
+print("----------------------")
+print(f"Winner: {winner}")
+print("----------------------")
